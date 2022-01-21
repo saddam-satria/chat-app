c77b65ef25b7d525bf748fcbd6d72c533f0c0f9f
saddam-satria <ardhisaddam@gmail.com>
2022-01-21 16:54:10 +0700

    modify pusher config

--- backend/src/app/config/pusher.ts
+++ backend/src/app/config/pusher.ts
@@ -1,9 +1,11 @@
 import Pusher from 'pusher';
+import { PUSHER_CLUSTER, PUSHER_ID, PUSHER_KEY, PUSHER_SECRET } from './constant';
 
-// const pusher = new Pusher({
-//   appId: process.env,
-//   key: 'APP_KEY',
-//   secret: 'SECRET_KEY',
-//   useTLS: USE_TLS, // optional, defaults to false
-//   cluster: 'CLUSTER', // if `host` is present, it will override the `cluster` option.
-// });
+const pusher = new Pusher({
+  appId: PUSHER_ID,
+  key: PUSHER_KEY,
+  secret: PUSHER_SECRET,
+  cluster: PUSHER_CLUSTER, // if `host` is present, it will override the `cluster` option.
+});
+
+export default pusher;
