696e6ac12b2f3588edaf665b4edac36dabe8c9e6
saddam-satria <ardhisaddam@gmail.com>
2022-01-21 16:35:38 +0700

    add mongoose connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,16 +2,27 @@ import express from 'express';
 import { config } from 'dotenv';
 import cors from 'cors';
 import routes from './app/config/routes';
+import { connect } from 'mongoose';
 
 (() => {
-  config();
   const app = express();
   const PORT: number | string = process.env.PORT || 5000;
+
+  config();
   app.use(cors());
   app.use(express.json());
   app.use(routes);
 
-  app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
-  });
+  const MONGO_URL: any = process.env.MONGO_URL;
+
+  connect(MONGO_URL)
+    .then(() => {
+      app.listen(PORT, () => {
+        console.log(`server running on port ${PORT}`);
+        console.log('mongo db connected');
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 })();
