d9180eedc8bce2be7941b945d27e9c4c551502e1
saddam-satria <ardhisaddam@gmail.com>
2022-01-21 18:02:02 +0700

    modify add new routing /api/v1/messages

--- backend/src/app/config/routes.ts
+++ backend/src/app/config/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import MessageController from '../controllers/messageController';
 import WelcomeController from '../controllers/welcomeController';
 
 const router = Router();
 
 router.get('/', (req, res) => new WelcomeController(req, res).get());
+router.get('/api/v1/messages', (req, res) => new MessageController(req, res).get());
+router.post('/api/v1/messages', (req, res) => new MessageController(req, res).post());
 
 export default router;
