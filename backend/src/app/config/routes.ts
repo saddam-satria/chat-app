b10f2597740fb407e16135940464aef586b5a15d
saddam-satria <ardhisaddam@gmail.com>
2022-01-22 14:17:26 +0700

    modify new routing message

--- backend/src/app/config/routes.ts
+++ backend/src/app/config/routes.ts
@@ -1,11 +1,21 @@
 import { Router } from 'express';
 import MessageController from '../controllers/messageController';
+import MessagesController from '../controllers/messagesController';
 import WelcomeController from '../controllers/welcomeController';
 
 const router = Router();
 
 router.get('/', (req, res) => new WelcomeController(req, res).get());
-router.get('/api/v1/messages', (req, res) => new MessageController(req, res).get());
-router.post('/api/v1/messages', (req, res) => new MessageController(req, res).post());
+
+router
+  .route('/api/v1/messages')
+  .get((req, res) => new MessagesController(req, res).get())
+  .post((req, res) => new MessagesController(req, res).post());
+
+router
+  .route('/api/v1/message/:id')
+  .get((req, res) => new MessageController(req, res).get())
+  .delete((req, res) => new MessageController(req, res).delete())
+  .put((req, res) => new MessageController(req, res).update());
 
 export default router;
