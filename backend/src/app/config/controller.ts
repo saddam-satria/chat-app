9592401da2e51848ab2b383c32514d1e457a13b1
saddam-satria <ardhisaddam@gmail.com>
2022-01-22 14:18:10 +0700

    modify error handling

--- backend/src/app/config/controller.ts
+++ backend/src/app/config/controller.ts
@@ -3,10 +3,18 @@ import { Request, Response } from 'express';
 class BaseController {
   protected request: Request;
   protected response: Response;
+  protected currentDate: string = new Date().toString();
 
   constructor(request: Request, response: Response) {
     this.request = request;
     this.response = response;
+  }
+  protected errorHandling(code: number, message?: string | any) {
+    this.response.status(code).json({
+      status: this.response.statusCode,
+      timestampts: this.currentDate,
+      message,
+    });
   }
 }
 
