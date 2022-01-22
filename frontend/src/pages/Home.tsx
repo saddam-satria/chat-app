ee57bfb9644d951a77d468511609f742e04e08f8
saddam-satria <ardhisaddam@gmail.com>
2022-01-22 14:11:33 +0700

    add react chakra ui

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, Heading } from '@chakra-ui/react';
+
+const Home: React.FC = () => {
+  return (
+    <Box>
+      <Heading as={'h3'} color={'tomato'}>
+        Hello
+      </Heading>
+    </Box>
+  );
+};
+
+export default Home;
