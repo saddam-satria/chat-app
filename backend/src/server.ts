30bd7d7f57217c807e50891f3a473ff3d7be47b7
saddam-satria <ardhisaddam@gmail.com>
2022-01-23 16:03:34 +0700

    modify change stream mongo

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './app/config/routes';
-import { connect } from 'mongoose';
+import mongoose from 'mongoose';
 import { MONGO_URL, PORT } from './app/config/constant';
+import Messages from './app/models/Messages';
+import pusher from './app/config/pusher';
 
 (() => {
   const app = express();
@@ -11,7 +13,15 @@ import { MONGO_URL, PORT } from './app/config/constant';
   app.use(express.json());
   app.use(routes);
 
-  connect(MONGO_URL)
+  Messages.watch().on('change', async (change) => {
+    const messages = change.fullDocument;
+    if (change.operationType === 'insert') {
+      await pusher.trigger('messages', 'send-message', messages);
+    }
+  });
+
+  mongoose
+    .connect(MONGO_URL)
     .then(() => {
       app.listen(PORT, () => {
         console.log(`server running on port ${PORT}`);
