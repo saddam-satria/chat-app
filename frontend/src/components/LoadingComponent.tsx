f84d161da04b6e03b0cb9f515a359bdee46cac16
saddam-satria <ardhisaddam@gmail.com>
2022-01-24 15:30:08 +0700

    add loading component

---
+++ frontend/src/components/LoadingComponent.tsx
@@ -0,0 +1,12 @@
+import { Text } from '@chakra-ui/react';
+import React from 'react';
+
+const LoadingComponent: React.FC = () => {
+  return (
+    <>
+      <Text color={'propulsion.thirdy'}>Loading</Text>
+    </>
+  );
+};
+
+export default LoadingComponent;
