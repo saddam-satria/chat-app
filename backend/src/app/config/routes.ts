7613d0a20815beac69954ba0a6b48d49875358b1
saddam-satria <ardhisaddam@gmail.com>
2022-01-24 18:41:36 +0700

    modify add new routing login & register

--- backend/src/app/config/routes.ts
+++ backend/src/app/config/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
+import LoginController from '../controllers/loginController';
 import MessageController from '../controllers/messageController';
 import MessagesController from '../controllers/messagesController';
+import RegisterController from '../controllers/registerController';
 import WelcomeController from '../controllers/welcomeController';
 
 const router = Router();
@@ -17,5 +19,8 @@ router
   .get((req, res) => new MessageController(req, res).get())
   .delete((req, res) => new MessageController(req, res).delete())
   .put((req, res) => new MessageController(req, res).update());
+
+router.route('/api/v1/register').post((req, res) => new RegisterController(req, res).post());
+router.route('/api/v1/login').post((req, res) => new LoginController(req, res).post());
 
 export default router;
